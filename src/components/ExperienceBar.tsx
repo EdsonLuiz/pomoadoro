2cb438fae0591adccd8d374488a724f536729f06
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-22 13:06:35 -0300

    refactor: remove unnecessary class

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,7 +1,7 @@
 export function ExperienceBar() {
   return (
     <header className="flex items-center">
-      <span className="text-xs">0 xp</span>
+      <span>0 xp</span>
       <div className="relative flex-1 h-1 mx-6 bg-gray-200 rounded">
         <div className="w-6/12 h-1 bg-lime-500">
           <span className="absolute transform -translate-x-1/2 top-3 left-1/2">300 xp</span>
