0d4d05743d4b8be03da8238271a5698d16885bd9
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-23 11:44:06 -0300

    feat: add Countdown component

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,50 @@
-export function Countdown() {
-  return(
-    <CountdownContainer>
+import { useEffect, useState } from "react";
 
-      <Display>
-        <Text>2</Text>
-        <Text>5</Text>
-      </Display>
-      
-      <span className="mx-1 text-8xl">:</span>
-      
-      <Display>
-        <Text>0</Text>
-        <Text>0</Text>
-      </Display>
-      
-    </CountdownContainer>
-  )
+
+export function Countdown() {
+
+  const [time, setTime] = useState(25 * 60)
+  const [active, setActive] = useState(false)
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const [minutesLeft, minutestRight] = String(minutes).padStart(2, '0').split('')
+  const [secondsLeft, secondstRight] = String(seconds).padStart(2, '0').split('')
+
+  function startCountdown() {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if(active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000);
+    }
+  }, [active, time])
+
+  return (
+    <>
+      <CountdownContainer>
+        <Display>
+          <Text>{minutesLeft}</Text>
+          <Text>{minutestRight}</Text>
+        </Display>
+
+        <span className="mx-1 text-8xl">:</span>
+
+        <Display>
+          <Text>{secondsLeft}</Text>
+          <Text>{secondstRight}</Text>
+        </Display>
+      </CountdownContainer>
+
+      <Button execute={startCountdown}>
+        Iniciar um ciclo
+      </Button>
+    </>
+  );
 }
 
 const CountdownContainer = ({children}) => (
@@ -33,3 +62,11 @@ const Text = ({children}) => (
     {children}
   </span>
 )
+
+const Button = ({ children, execute }) => (
+  <button 
+    onClick={execute}
+    className="flex items-center justify-center flex-1 w-full h-20 mt-8 text-xl font-semibold text-white transition-colors duration-300 bg-indigo-500 rounded font-inter hover:bg-indigo-700">
+    {children}
+  </button>
+);
