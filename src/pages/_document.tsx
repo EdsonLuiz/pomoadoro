5b3db37f7de720a144949abddf8317d6ee7f25ef
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-23 09:26:22 -0300

    refactor: add more font weight

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,8 +17,7 @@ import Document, {
       return (
         <Html lang="pt-br">
           <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@400;500;600&display=swap" rel="stylesheet" /> 
           </Head>
           <body>
             <Main />
