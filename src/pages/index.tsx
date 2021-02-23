6677063024aafdc7b86b80d97ab940ba4ae93d93
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-23 08:55:23 -0300

    refactor: remove unnused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,6 @@ import { ExperienceBar } from "../components/ExperienceBar";
 
 export default function Home() {
   return (
-      // {/* <Head>
-      //   <title>Create Next App</title>
-      //   <link rel="icon" href="/favicon.ico" />
-      // </Head> */}
       <div className="container flex flex-col h-screen px-8 py-10 mx-auto">
         <ExperienceBar />
       </div>
