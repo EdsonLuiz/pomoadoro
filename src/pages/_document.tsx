e13a97a208cccc6024101deda88f6a1db60e61aa
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-23 08:51:28 -0300

    refactor: add lang attribute to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ import Document, {
   
     render() {
       return (
-        <Html>
+        <Html lang="pt-br">
           <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
