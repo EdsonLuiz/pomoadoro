b51eb80d72f578e4f4836a1f2c5c298ba1b18f14
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-23 09:41:57 -0300

    refactor: stract span to XpCounter component

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,13 +1,15 @@
+import { XpCounter } from "./XpCounter";
+
 export function ExperienceBar() {
   return (
     <header className="flex items-center">
-      <span>0 xp</span>
+      <XpCounter>0 xp</XpCounter>
       <div className="relative flex-1 h-1 mx-6 bg-gray-200 rounded">
         <div className="w-6/12 h-1 bg-lime-500">
-          <span className="absolute transform -translate-x-1/2 top-3 left-1/2">300 xp</span>
+          <span className="absolute transform -translate-x-1/2 text-warmGray-500 font-inter top-3 left-1/2">300 xp</span>
         </div>
       </div>
-      <span>600 xp</span>
+      <XpCounter>600 xp</XpCounter>
     </header>
   )
 }
---
+++ src/components/XpCounter.tsx
@@ -0,0 +1,5 @@
+export function XpCounter({children}) {
+  return (
+    <span className="text-sm font-normal font-inter text-warmGray-500">{children}</span>
+  )
+}
