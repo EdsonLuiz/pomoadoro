6bd68a4bfff22fe8d54e0a6ae78e15f6bf28d39c
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-23 10:05:13 -0300

    feat: Create component CompletedChallenges

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,8 @@
+export function CompletedChallenges() {
+  return (
+    <div className="flex items-center justify-between pb-4 font-medium border-b border-gray-300 font-inter my-14 text-warmGray-500">
+      <span className="text-xl">Desafios completos</span>
+      <span className="text-xl">5</span>
+    </div>
+  )
+}
