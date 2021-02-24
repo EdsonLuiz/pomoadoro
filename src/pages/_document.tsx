feea46decbd9c628c5997265bbfa3b3042e19654
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-24 16:58:18 -0300

    chore: change googlefonts loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,32 @@ import Document, {
         <Html lang="pt-br">
           <Head>
             <link rel="shortcut icon" href="favicon.png" type="image/png"/>
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@400;500;600&display=swap" rel="stylesheet" /> 
+
+            <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@400;500;600&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@400;500;600&display=swap"
+            media="print"
+            // @ts-ignore
+            onLoad="this.media='all'"
+          />
+          <noscript>
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@400;500;600&display=swap"
+            />
+          </noscript>
+
+
           </Head>
           <body className="bg-gray-100 dark:bg-trueGray-800">
             <Main />
