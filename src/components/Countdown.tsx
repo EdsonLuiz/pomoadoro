c204dc9606d19635305f53adb4915cf6afdaf963
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-24 08:43:45 -0300

    refactor: improve naming convention

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 export function Countdown() {
 
   const [time, setTime] = useState(25 * 60)
-  const [active, setActive] = useState(false)
+  const [isActive, setIsActive] = useState(false)
 
   const minutes = Math.floor(time / 60)
   const seconds = time % 60
@@ -13,16 +13,16 @@ export function Countdown() {
   const [secondsLeft, secondstRight] = String(seconds).padStart(2, '0').split('')
 
   function startCountdown() {
-    setActive(true)
+    setIsActive(true)
   }
 
   useEffect(() => {
-    if(active && time > 0) {
+    if(isActive && time > 0) {
       setTimeout(() => {
         setTime(time - 1)
       }, 1000);
     }
-  }, [active, time])
+  }, [isActive, time])
 
   return (
     <>
