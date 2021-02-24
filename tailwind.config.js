d329f49ab49db7a8be88bf136997388d98e67515
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-23 21:16:02 -0300

    chore: make trueGray available in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
       inter: ['Inter', 'sans-serif'],
@@ -13,6 +13,7 @@ module.exports = {
       ...defaultTheme.colors,
       lime: colors.lime,
       warmGray: colors.warmGray,
+      trueGray: colors.trueGray,
       blueGray: colors.blueGray,
       rose: colors.rose,
       lightBlue: colors.lightBlue,
