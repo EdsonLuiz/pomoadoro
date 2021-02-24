080063da103d41dbd37b231858fe938aa3a52c46
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-24 15:37:22 -0300

    refactor: get current experience from ChallengeContext

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,15 @@
+import { useContext } from "react";
+import { ChallengeContext } from "../contexts/ChallengesContext";
 import { XpCounter } from "./XpCounter";
 
 export function ExperienceBar() {
+  const {currentExperience} = useContext(ChallengeContext)
   return (
     <header className="flex items-center col-span-full">
       <XpCounter>0 xp</XpCounter>
       <div className="relative flex-1 h-1 mx-6 bg-gray-200 rounded">
         <div className="w-6/12 h-1 bg-lime-500">
-          <span className="absolute transform -translate-x-1/2 dark:text-gray-100 text-warmGray-500 font-inter top-3 left-1/2">300 xp</span>
+          <span className="absolute transform -translate-x-1/2 dark:text-gray-100 text-warmGray-500 font-inter top-3 left-1/2">{currentExperience} xp</span>
         </div>
       </div>
       <XpCounter>600 xp</XpCounter>
