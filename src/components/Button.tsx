34dac1b726a01a6150aef56aeaec6f848edcf6cf
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-26 14:40:55 -0300

    refactor: remove unnused css classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export function Button () {
   const {resertCountdown, startCountdown, isActive, hasFinished} = useContext(CountdownContext);
 
   let cursor = hasFinished ? 'cursor-not-allowed' : ''
-  const baseStyle = `${cursor} flex items-center justify-center flex-1 w-full h-16 mt-6 text-lg font-semibold text-white transition-colors duration-300 bg-indigo-500 rounded sm:h-20 sm:mt-8 sm:text-xl font-inter hover:bg-indigo-700`
+  const baseStyle = `${cursor} flex items-center justify-center w-full h-16 mt-6 text-lg font-semibold text-white transition-colors duration-300 bg-indigo-500 rounded sm:h-20 sm:mt-8 sm:text-xl font-inter hover:bg-indigo-700`
 
   const executeAction = isActive ? resertCountdown : startCountdown
   return (
