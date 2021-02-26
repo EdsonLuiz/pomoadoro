1d4a2b0250320e524bd15295e326b0febde60d51
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-26 16:19:56 -0300

    refactor: clear Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,5 @@
 export function Container({children}) {
   return (
-    // <div className="container flex flex-col h-screen px-8 py-10 mx-auto">
-    //   {children}
-    // </div>
     <div className="container grid h-screen grid-cols-1 p-4 mx-auto sm:px-8 sm:py-10 ">
       {children}
     </div>
