44583867ae6966557db99507ca443a06ac59a7e5
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-26 11:44:31 -0300

    chore: configure tailwind to show image as background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'levelup': "url('/icons/levelup.svg')",
+       })
+    },
     fontFamily: {
       inter: ['Inter', 'sans-serif'],
       rajdhani: ['Rajdhani', 'sans-serif'],
